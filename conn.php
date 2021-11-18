920ebeace3e892c1c2a91e4a55309701e84d8482
nganduc190100 <74098915+nganduc190100@users.noreply.github.com>
2021-11-18 20:14:15 +0700

    conn.php
    abc

---
+++ conn.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+ $username ="root";
+ $password="";
+ $database = "mywebsite";
+  //Khai báo biến để kết nối đến CSDL
+ $connect = mysqli_connect($servername, $username,$password,$database);
+ if(!$connect){
+ 	echo"Kết nối thất bại";
+ }
+ 
+?>
